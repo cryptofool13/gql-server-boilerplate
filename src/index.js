449889c6c86ebf798bcc3ff41df60d1bcb3a7130
mike <fender18295@gmail.com>
2019-05-24 15:23:43 -0400

    added sql

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import express from "express";
+import "dotenv/config";
 import cors from "cors";
-import { ApolloServer } from "apollo-server-express";
 import uuidv4 from "uuid/v4";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
 
 import schema from "./schema";
 import resolvers from "./resolvers";
-import models from "./models";
+import models, { sequelize } from "./models";
 
 const app = express();
 
@@ -14,14 +15,67 @@ app.use(cors());
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
-  context: {
+  formatError: error => {
+    // remove the internal sequelize error message
+    // leave only the important validation error
+    const message = error.message
+      .replace("SequelizeValidationError: ", "")
+      .replace("Validation error: ", "");
+
+    return {
+      ...error,
+      message
+    };
+  },
+  context: async () => ({
     models,
-    me: models.users[1]
-  }
+    me: await models.User.findByLogin("mike")
+  })
 });
 
 server.applyMiddleware({ app, path: "/graphql" });
 
-app.listen({ port: 3000 }, () => {
-  console.log(`Apollo server on http://localhost:3000/graphql`);
+const eraseDatabaseOnSync = true;
+
+sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
+  if (eraseDatabaseOnSync) {
+    createUsersWithMessages();
+  }
+
+  app.listen({ port: 8000 }, () => {
+    console.log("Apollo Server on http://localhost:8000/graphql");
+  });
 });
+
+const createUsersWithMessages = async () => {
+  await models.User.create(
+    {
+      username: "mike",
+      messages: [
+        {
+          text: "Published the Road to learn React"
+        }
+      ]
+    },
+    {
+      include: [models.Message]
+    }
+  );
+
+  await models.User.create(
+    {
+      username: "alex",
+      messages: [
+        {
+          text: "Happy to release ..."
+        },
+        {
+          text: "Published a complete ..."
+        }
+      ]
+    },
+    {
+      include: [models.Message]
+    }
+  );
+};
--- src/resolvers/message.js
+++ src/resolvers/message.js
@@ -2,41 +2,50 @@ import uuidv4 from "uuid/v4";
 
 export default {
   Query: {
-    messages: (parent, args, { models }) => {
-      return Object.values(models.messages);
+    messages: async (parent, args, { models }) => {
+      return await models.Message.findAll(); // Object.values(models.messages);
     },
-    message: (parent, { id }, { models }) => {
-      return models.messages[id];
+    message: async (parent, { id }, { models }) => {
+      return await models.Message.findByPk(id); // models.messages[id];
     }
   },
   Mutation: {
-    createMessage: (parent, { text }, { me, models }) => {
-      const id = uuidv4();
-      const message = {
-        id,
-        text,
-        userId: me.id
-      };
-
-      models.messages[id] = message;
-      models.users[me.id].messageIds.push(id);
-      return message;
-    },
-    deleteMessage: (parent, { id }, { models }) => {
-      const { [id]: message, ...otherMessages } = models.messages;
-
-      if (!messages) {
-        return false;
+    createMessage: async (parent, { text }, { me, models }) => {
+      try {
+        return await models.Message.create({
+          text,
+          userId: me.id
+        });
+      } catch (error) {
+        throw new Error(error);
       }
-      models.messages = otherMessages;
+      // const id = uuidv4();
+      // const message = {
+      //   id,
+      //   text,
+      //   userId: me.id
+      // };
 
-      return true;
+      // models.messages[id] = message;
+      // models.users[me.id].messageIds.push(id);
+      // return message;
+    },
+    deleteMessage: async (parent, { id }, { models }) => {
+      return await models.Message.destroy({ where: { id } });
     }
+    // const { [id]: message, ...otherMessages } = models.messages;
+
+    // if (!models.messages) {
+    //   return false;
+    // }
+    // models.messages = otherMessages;
+
+    // return true;
   },
 
   Message: {
-    user: (message, args, { models }) => {
-      return models.users[message.userId];
+    user: async (message, args, { models }) => {
+      return await models.User.findByPk(message.userId); // models.users[message.userId];
     }
   }
 };
